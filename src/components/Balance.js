78a301520638f73a1d6a2df96212add5037790b6
ajay <akrishnan1999bs@gmail.com>
2021-11-06 19:10:27 +0530

    button css

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -3,8 +3,8 @@ import React from "react";
 const Balance = ({income,out}) => {
   return (
     <>
-      <h4>Your Balance</h4>
-      <h1 className="balance">$ {income+out}</h1>
+      <h4 style={{color:"white"}}>Your Balance</h4>
+      <h1 className="balance">$ {income + out}</h1>
     </>
   );
 };
